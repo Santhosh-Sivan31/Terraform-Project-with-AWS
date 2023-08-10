ff9b9185da6df95dfa1bf22a84046da3be31cd76
Santhosh Sivan <121418181+Santhosh-Sivan31@users.noreply.github.com>
2023-08-10 13:35:44 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,140 @@
+resource "aws_vpc" "newvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.newvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.newvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.newvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.newvpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "websg" {
+  name   = "websg"
+  vpc_id = aws_vpc.newvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "websg"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "myuniques3bucket1999"
+}
+
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-053b0d53c279acc90"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id              = aws_subnet.subnet1.id
+  user_data              = base64encode(file("userdata.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-053b0d53c279acc90"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id              = aws_subnet.subnet2.id
+  user_data              = base64encode(file("userdata2.sh"))
+}
+
+resource "aws_lb" "newalb" {
+  name               = "newalb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.websg.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  tags = {
+    Name = "newalb"
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "newtg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.newvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.newalb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.tg.arn
+    type             = "forward"
+  }
+}
+
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "loadbalancerdns" {
+  value = aws_lb.newalb.dns_name
+}
